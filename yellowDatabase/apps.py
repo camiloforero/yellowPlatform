9cc6f4409a9cd8995e56bddbcb78abf63dd8b345
Camilo Forero <ce.forero2551@uniandes.edu.co>
2016-01-10 14:08:56 -0500

    Initial commit, created the bare structure of the yellow platform using Django 1.9 and Python 2.7

---
+++ yellowDatabase/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class YellowdatabaseConfig(AppConfig):
+    name = 'yellowDatabase'
---
+++ yellowEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/var/www/natco2015/yellowEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
